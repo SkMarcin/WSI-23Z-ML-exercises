b9e50aea3c6549440c4992f4ed43b7cb222aca62
msikorsk <01178592@pw.edu.pl>
2023-12-11 20:33:24 +0100

    Added function to get class from forest and to get model accuracy, recall and precision

--- main.py
+++ main.py
@@ -1,32 +1,70 @@
 from ucimlrepo import fetch_ucirepo
-from training import create_forest
+from training import create_forest, get_forest_classifier, get_classification_dict, get_accuracy, get_recall, get_precision
 import numpy as np
 
-SPLIT = 1000
-TREES = 100
+SPLIT = -1000
+TREES = 300
+CLASSES = ["unacc", "acc", "good", "vgood"]
 
-# fetch dataset
-car_evaluation = fetch_ucirepo(id=19)
+# # fetch dataset
+# car_evaluation = fetch_ucirepo(id=19)
 
-# data (as pandas dataframes)
-X = car_evaluation.data.features
-Y = car_evaluation.data.targets
+# # data (as pandas dataframes)
+# X = car_evaluation.data.features
+# Y = car_evaluation.data.targets
 
-targets = [Y.iloc[i, 0] for i in range(0, 1728)]
+# targets = [Y.iloc[i, 0] for i in range(0, 1728)]
+
+# data = []
+# for i in range(0, 1728):
+#     row = []
+#     for k in range(0, 6):
+#         row.append(X.iloc[i, k])
+#     data.append(row)
+
+# if SPLIT > len(data):
+#     raise ValueError
+
+# file = open("data.txt", "w")
+# for row in data:
+#     file.write(f'{str(row)}\n')
+# file.close()
+# file = open("targets.txt", "w")
+# for row in targets:
+#     file.write(f'{str(row)}\n')
+# file.close()
 
 data = []
-for i in range(0, 1728):
-    row = []
-    for k in range(0, 6):
-        row.append(X.iloc[i, k])
-    data.append(row)
+targets = []
 
-if SPLIT > len(data):
-    raise ValueError
+file = open("data.txt", "r")
+for line in file:
+    line = line.strip(" \n[]")
+    line = line.split(", ")
+    data_line = [element.strip("'") for element in line]
+    data.append(data_line)
+file.close()
+file = open("targets.txt", "r")
+for element in file:
+    element = element.strip(" \n")
+    targets.append(element)
+file.close()
 
-training_data = data[SPLIT:]
-testing_data = data[:SPLIT]
-training_targets = targets[SPLIT:]
-testing_targets = targets[:SPLIT]
+# [:2] - do 2 elementu wyłącznie, [2:] od 2 elementu włącznie
+training_data = data[:SPLIT]
+testing_data = data[SPLIT:]
+training_targets = targets[:SPLIT]
+testing_targets = targets[SPLIT:]
 
 forest = create_forest(training_data, training_targets, TREES)
+
+results = []
+for index, row in enumerate(testing_data):
+    print(index)
+    results.append((testing_targets[index], get_forest_classifier(forest, row)))
+
+dictionary = get_classification_dict(results)
+print(round(get_accuracy(dictionary, CLASSES), 2))
+print(round(get_recall(dictionary, CLASSES), 2))
+print(round(get_precision(dictionary, CLASSES), 2))
+print("finished")
--- training.py
+++ training.py
@@ -2,7 +2,7 @@ from decision_trees import DecisionTree, copy
 from random import choice, randint
 import numpy as np
 
-N = 50
+N = 250
 # array[row][column]
 # array[[column, column, column], row, row]
 
@@ -16,7 +16,6 @@ def create_forest(data, targets, trees_amount):
             chosen_data.append(data[index])
             chosen_targets.append(targets[index])
     
-        print(len(chosen_data[0]))
         attributes_selected = [False] * len(chosen_data[0])
         removed_atr = len(chosen_data[0]) - int(np.sqrt(len(chosen_data[0])))
         for atr in range(removed_atr):
@@ -29,5 +28,70 @@ def create_forest(data, targets, trees_amount):
 
     return trees
     
+def get_forest_classifier(forest, attributes):
+    classes_dict = {}
+    for tree in forest:
+        classifier = tree.get_prediction(attributes)
+        if classifier is None:
+            continue
+        if classifier in classes_dict:
+            classes_dict[classifier] += 1
+        else:
+            classes_dict[classifier] = 1
+    
+    max_classifier = None
+    max_value = float('-inf')
+    for classifier in classes_dict:
+        if classes_dict[classifier] > max_value:
+            max_classifier = classifier
+            max_value = classes_dict[classifier]
+    return max_classifier
 
 
+def get_classification_dict(tuples):
+    dict = {('unacc', 'unacc'): 0, ('unacc', 'acc'): 0, ('unacc', 'good'): 0, ('unacc', 'vgood'): 0, 
+            ('acc', 'unacc'): 0, ('acc', 'acc'): 0, ('acc', 'good'): 0, ('acc', 'vgood'): 0,
+            ('good', 'unacc'): 0, ('good', 'acc'): 0, ('good', 'good'): 0, ('good', 'vgood'): 0,
+            ('vgood', 'unacc'): 0, ('vgood', 'acc'): 0, ('vgood', 'good'): 0, ('vgood', 'vgood'): 0}
+    for tuple in tuples:
+        dict[tuple] += 1
+    return dict
+
+def get_accuracy(dictionary, classes):
+    correct = 0
+    incorrect = 0
+    for classifier in classes:
+        for key in dictionary:
+            if key[0] == key[1]:
+                correct += dictionary[key]
+            else:
+                incorrect += dictionary[key]
+    return correct / (correct + incorrect)
+
+def get_recall(dictionary, classes):
+    recalls = []
+    for classifier in classes:
+        correct = 0
+        total = 0
+        for key in dictionary:
+            if key[0] == classifier:
+                if key[1] == classifier:
+                    correct += dictionary[key]
+                total += dictionary[key]
+        if total > 0:
+            recalls.append(correct/total)
+    return sum(recalls) / len(recalls)
+
+def get_precision(dictionary, classes):
+    precisions = []
+    for classifier in classes:
+        correct = 0
+        total = 0
+        for key in dictionary:
+            if key[1] == classifier:
+                if key[0] == classifier:
+                    correct += dictionary[key]
+                total += dictionary[key]
+        if total > 0:
+            precisions.append(correct/total)
+    return sum(precisions) / len(precisions)
