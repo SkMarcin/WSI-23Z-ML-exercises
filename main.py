bcb408b1bac97434f3a42d60cee53c330d7375c1
msikorsk <01178592@pw.edu.pl>
2023-12-16 01:01:14 +0100

    Reading data from dataset

---
+++ main.py
@@ -0,0 +1,17 @@
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_openml
+from neural_net import NeuralNet
+import numpy as np
+
+LEARNING_SPEED = 0.1
+
+mnist = fetch_openml('mnist_784', version=1)
+
+data = np.array(mnist.data)
+targets = np.array(mnist.target)
+
+
+training_data, temp_data, training_targets, temp_targets = train_test_split(data, targets, test_size=0.2, random_state=1)
+testing_data, validation_data, testing_targets, validation_targets = train_test_split(temp_data, temp_targets, test_size=0.5, random_state=1)
+
+
