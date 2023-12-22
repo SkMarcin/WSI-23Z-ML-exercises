1cbe9a08e899bb63d341aff26e03dff5bee44a20
msikorsk <01178592@pw.edu.pl>
2023-12-22 21:54:32 +0100

    Wrote back propagation part of algorithm and added necessary attributes to neurons

--- neural_net.py
+++ neural_net.py
@@ -11,22 +11,25 @@ class NeuralNet:
         self.deep_layers = []
         self.output_layer = []
 
-    def activation_function(value):
+    def activation_function(self, value):
         return 1 / (1 + np.exp(-value))
+
+    def activation_derivative(self, value):
+        return self.activation_function(value) * (1 - self.activation_function(value))
 
     def create_net(self, deep_layer_count, deep_neuron_count):
         """
         This method creates the structure of NeuralNet
-        
+
         input_layer contains InputNeurons with only a value
-        
+
         deep_layers contains deep_layer_count layers with deep_neuron_count DeepNeurons each.
-        
+
         output_layer contains as many neurons as there are unique classes in targets
         """
         for _ in range(len(self.data[0])):
             self.input_layer.append(InputNeuron())
-        
+
         for i in range(deep_layer_count):
             self.deep_layers.append([])
             if i == 0:
@@ -39,11 +42,11 @@ class NeuralNet:
                     weights = [random() for _ in range(deep_neuron_count)]
                     bias = random()
                     self.deep_layers[i].append(DeepNeuron(weights, bias))
-        
+
         if deep_layer_count == 0:
             # when there are no deep layers
             deep_neuron_count = len(self.input_layer)
-    
+
         for _ in range(len(np.unique(self.targets))):
             weights = [random() for _ in range(deep_neuron_count)]
             bias = random()
@@ -51,18 +54,21 @@ class NeuralNet:
 
     def train_cycle(self):
         # forward propagation part
-        index = randint(0, len(self.data))
+        index = randint(0, len(self.data) - 1)
         self.load_pixels(index)
+        target_class = self.targets[index]
 
         prev_layer_values = []
         this_layer_values = []
         for neuron in self.input_layer:
             prev_layer_values.append(neuron.value)
 
-        for i, layer in enumerate(self.deep_layers):
+        for layer in self.deep_layers:
             for neuron in layer:
                 neuron.value = np.dot(neuron.weights, prev_layer_values) + neuron.bias
                 neuron.value = self.activation_function(neuron.value)
+                neuron.derivative_value = self.activation_derivative(neuron.value)  # this is used
+                neuron.weights_derivatives = np.array(prev_layer_values)            # in back propagation
                 this_layer_values.append(neuron.value)
             prev_layer_values = this_layer_values
             this_layer_values = []
@@ -70,15 +76,78 @@ class NeuralNet:
         for neuron in self.output_layer:
             neuron.value = np.dot(neuron.weights, prev_layer_values) + neuron.bias
             neuron.value = self.activation_function(neuron.value)
+            neuron.derivative_value = self.activation_derivative(neuron.value)      # this is used
+            neuron.weights_derivatives = np.array(prev_layer_values)                # in back propagation
             this_layer_values.append(neuron.value)
 
-        #back propagation part
+        # calculating error
+        probabilities = self.softmax()
+        errors = self.cross_entropy(probabilities, target_class)
 
+        for index, error in enumerate(errors):
+            self.output_layer[index].error = error
+
+        # back propagation part
+        next_layer_weights = []
+        next_layer_errors = []
+        for neuron in self.output_layer:
+            next_layer_weights.append(neuron.weights)
+            next_layer_errors.append(neuron.error)
+
+        # update output_layer parameters
+        for neuron in self.output_layer:
+            weights_gradients = neuron.weights_derivatives * neuron.derivative_value * neuron.error
+            bias_gradient = neuron.derivative_value * neuron.error
+            neuron.weights = neuron.weights - self.learning_speed * weights_gradients
+            neuron.bias = neuron.bias - self.learning_speed * bias_gradient
+
+        for layer in reversed(self.deep_layers):
+            # propagate error
+            for neuron_index, neuron in enumerate(layer):
+                neuron.error = 0
+                for error_index, error in enumerate(next_layer_errors):
+                    neuron.error += error * next_layer_weights[error_index][neuron_index]
+
+            next_layer_weights = []
+            next_layer_errors = []
+            for neuron in layer:
+                next_layer_weights.append(neuron.weights)
+                next_layer_errors.append(neuron.error)
+
+            # update layer parameters
+            for neuron in layer:
+                weights_gradients = neuron.weights_derivatives * neuron.derivative_value * neuron.error
+                bias_gradient = neuron.derivative_value * neuron.error
+                neuron.weights = neuron.weights - self.learning_speed * weights_gradients
+                neuron.bias = neuron.bias - self.learning_speed * bias_gradient
 
 
     def load_pixels(self, index):
         """this method inputs pixel data from index into input layer"""
 
-        for pixel in self.data[index]:
-            self.input_layer[index].value = pixel
-    
+        for pixel_index, pixel in enumerate(self.data[index]):
+            self.input_layer[pixel_index].value = pixel
+
+    def softmax(self):
+        logits_sum = 0
+        probabilities = []
+        for neuron in self.output_layer:
+            logits_sum += np.exp(neuron.value)
+
+        for neuron in self.output_layer:
+            probabilities.append(np.exp(neuron.value) / logits_sum)
+
+        return probabilities
+
+    def cross_entropy(self, probabilities, target_class):
+        epsilon = 1e-12
+
+        errors = []
+        for index, probabilty in enumerate(probabilities):
+            probabilty = np.clip(probabilty, epsilon, 1)
+            if index == target_class:
+                errors.append(-probabilty * np.log(probabilty))
+            else:
+                errors.append(0)
+
+        return errors
--- neurons.py
+++ neurons.py
@@ -3,6 +3,7 @@ class _Neuron:
     def __init__(self):
         self.value = 0
 
+
 class InputNeuron(_Neuron):
     def __init__(self):
         super().__init__()
@@ -10,12 +11,17 @@ class InputNeuron(_Neuron):
 class DeepNeuron(_Neuron):
     def __init__(self, weights, bias):
         super().__init__()
+        self.derivative_value = 0
+        self.weights_derivatives = []
         self.weights = weights
         self.bias = bias
-        
+        self.error = 0
+
 class OutputNeuron(_Neuron):
     def __init__(self, weights, bias):
         super().__init__()
+        self.derivative_value = 0
+        self.weights_derivatives = []
         self.weights = weights
         self.bias = bias
-        
+        self.error = 0
