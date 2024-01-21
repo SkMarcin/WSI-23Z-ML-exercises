f0c35f708b274419da03e66bfacca2f78fd6d2b9
msikorsk <01178592@pw.edu.pl>
2024-01-21 21:51:43 +0100

    wrote algorithm

--- bayes.py
+++ bayes.py
@@ -1,17 +1,60 @@
 import numpy as np
 
 class BayesClassifier:
-    def __init__(self, ):
-        pass
+    def __init__(self, data, targets):
+        self.data = data
+        self.targets = targets
 
-    def fit(self, data, targets):
-        self.targets = np.unique(targets)
+    def calculate_priors(self):
+        targets = np.unique(self.targets)
         self.class_priors = {}
-        self.class_means = {}
-        self.class_variances = {}
 
-        for target in self.targets:
-            for row in data:
-                self.class_priors[target] += 1
-                self.class_means[target] = np.mean(X_c, axis=0)
-                self.class_variances[target] = np.var(X_c, axis=0)
+        for target in targets:
+            for index, row in enumerate(self.data):
+                if self.targets[index] == target:
+                    if target in self.class_priors:
+                        self.class_priors[target] += 1
+                    else:
+                        self.class_priors[target] = 1
+
+            self.class_priors[target] /= len(self.targets)
+
+    def calculate_prob(self, feature_index, feature_value, target):
+        feature_values = []
+        for index, row in enumerate(self.data):
+            if self.targets[index] == target:
+                feature_values.append(row[feature_index])
+
+        mean = np.mean(feature_values)
+        dev = np.std(feature_values)
+
+        part1 = (1 / np.sqrt(2 * np.pi * np.square(dev)))
+        part2 = np.exp(-((np.square(feature_value - mean)) / (2 * np.square(dev))))
+        return part1 * part2
+
+    def train(self):
+        self.calculate_priors()
+
+        predictions = []
+        for row in self.data:
+            probabilities = [1] * len(self.class_priors)
+            for prior_index, prior in enumerate(self.class_priors):
+                for feature_index, feature in enumerate(row):
+                    probabilities[prior_index] *= self.calculate_prob(feature_index, feature, prior)
+
+            post_probabilities = [1] * len(self.class_priors)
+            for prior_index, prior in enumerate(self.class_priors):
+                post_probabilities[prior_index] = probabilities[prior_index] * self.class_priors[prior]
+
+            predictions.append(np.argmax(post_probabilities))
+
+        return predictions
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -7,27 +7,38 @@ file_handle = open("seeds_dataset.txt", "r")
 data = []
 targets = []
 
+# for line in file_handle:
+#     line = line.strip(" \n[]")
+#     line = line.split("\t")
+#     line = [element for element in line if element]
+#     data_line = []
+#     for i in range(7):
+#         data_line.append(float(line[i]))
+
+#     data_line.append(line[7])
+#     data.append(data_line)
+#     targets.append(line[7])
+
+
+# df = pd.DataFrame(data, columns=['area', 'perimeter', 'compactness', 'length', 'width', 'assymetry_coeff', 'groove_length', 'class'])
+# corr = df.iloc[:,:-1].corr(method="pearson")
+# cmap = sns.diverging_palette(250,354,80,60,center='dark',as_cmap=True)
+# sns.heatmap(corr, vmax=1, vmin=-0.5, cmap=cmap, square=True, linewidths=.2)
+
+
 for line in file_handle:
     line = line.strip(" \n[]")
     line = line.split("\t")
     line = [element for element in line if element]
     data_line = []
-    for i in range(7):
-        data_line.append(float(line[i]))
-
-    data_line.append(line[7])
+    data_line.append(float(line[2]))
+    data_line.append(float(line[5]))
+    data_line.append(float(line[6]))
     data.append(data_line)
     targets.append(line[7])
 
-print(data)
-print(targets)
 
-df = pd.DataFrame(data, columns=['area', 'perimeter', 'compactness', 'length', 'width', 'assymetry_coeff', 'groove_length', 'class'])
-corr = df.iloc[:,:-1].corr(method="pearson")
-cmap = sns.diverging_palette(250,354,80,60,center='dark',as_cmap=True)
-sns.heatmap(corr, vmax=1, vmin=-0.5, cmap=cmap, square=True, linewidths=.2)
-
-bayes = BayesClassifier()
-# bayes.fit(data, targets)
+bayes = BayesClassifier(data, targets)
+results = bayes.train()
 
 print('fin')
